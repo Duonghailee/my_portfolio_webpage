f28739f0f84ba677d4bd05457e3f82c579911d62
duonghailee <duonghailee@gmail.com>
2017-07-19 00:35:39 +0300

    modify weather API to suit codepen environment

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
     // Add smooth scrolling to all links in navbar + footer link
-    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-
+    $(".navbar a, footer a[href='#myPage']").on("click", function(event) {
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-
             // Prevent default anchor click behavior
             event.preventDefault();
 
@@ -13,19 +11,19 @@ $(document).ready(function() {
 
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 900, function() {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
+            $("html, body").animate({
+                    scrollTop: $(hash).offset().top
+                },
+                900,
+                function() {
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                }
+            );
         } // End if
     });
     $(window).scroll(function() {
         $(".slideanim").each(function() {
-
-
             // slide target
             var pos = $(this).offset().top;
 
@@ -34,38 +32,46 @@ $(document).ready(function() {
                 $(this).addClass("slide");
             }
         });
-
     });
-
-
-
-
-
 });
 
 // get current time and display at the bottom of the page
 function getTime() {
     var currentDate = new Date();
-    var date_time = currentDate.getDate() + "/" + (currentDate.getMonth() + 1) + "/" + currentDate.getFullYear() + "@" +
-        currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+    var date_time =
+        currentDate.getDate() +
+        "/" +
+        (currentDate.getMonth() + 1) +
+        "/" +
+        currentDate.getFullYear() +
+        "@" +
+        currentDate.getHours() +
+        ":" +
+        currentDate.getMinutes() +
+        ":" +
+        currentDate.getSeconds();
     var showtime = document.getElementById("show_time");
 
-    document.getElementById("show_time").innerHTML = "<h3>Local time (Helsinki) is now:  " + date_time + "</h3>";
+    document.getElementById("show_time").innerHTML =
+        "<h3>Local time (Helsinki) is now:  " + date_time + "</h3>";
 
     //move target to bottom, slow the motion for 2800ms before going back to about navigation
-    $('html, body').animate({ scrollTop: $('#show_time').offset().top }, 2800);
-
-
-};
+    $("html, body").animate({ scrollTop: $("#show_time").offset().top }, 2800);
+}
 
 //get weather api
 function get_weather() {
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Helsinki,Finland&&APPID=b9d8c6dc9a699b32bff4368ac7696ef1", function(data) {
-        $('#show_weather').html('Helsinki, Finland temperature: ' + (data.main.temp - 273.15).toFixed(1) + '<sup>o</sup>C');
-    });
-};
-
-
+    $.getJSON(
+        "https://fcc-weather-api.glitch.me/api/current?lat=60&lon=25",
+        function(data) {
+            $("#show_weather").html(
+                "Helsinki, Finland temperature: " + data.main.temp + "<sup>o</sup>C"
+            );
+            $("#status").html("<h4>" + data.weather[0].description + "</h4>");
+            $("#image").attr("src", data.weather[0].icon);
+        }
+    );
+}
 
 // send validation
 function check_form() {
@@ -81,8 +87,8 @@ function check_form() {
 
     alert("Thanks for contacting me");
     return true;
-};
+}
 
 function reset() {
     document.getElementById("form").reset();
-};
+}
